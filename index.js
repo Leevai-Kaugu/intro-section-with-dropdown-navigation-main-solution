a42800d6f5d49c5986d5c564a315f0a3853b05c4
Leevai-Kaugu <125732303+Leevai-Kaugu@users.noreply.github.com>
2024-10-01 23:21:33 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hamButton = document.querySelector('.hamIcon');
 const menu = document.querySelector('.linksGroup');
 const closeButton = document.querySelector('.closeIcon');
+const features = document.querySelector('.Features');
+const company = document.querySelector('.company');
 
 
 hamButton.addEventListener("click",()=>{
@@ -11,4 +13,24 @@ hamButton.addEventListener("click",()=>{
 closeButton.addEventListener("click",()=>{
   hamButton.style.display = 'flex';
   menu.style.display = 'none';
+});
+features.addEventListener("mouseenter",()=>{
+  document.querySelector('.featuresList').style.display = 'flex';
+  document.querySelector('.downIcon').style.display = 'none';
+  document.querySelector('.upIcon').style.display = 'block';
+});
+features.addEventListener("mouseleave",()=>{
+  document.querySelector('.featuresList').style.display = 'none';
+document.querySelector('.upIcon').style.display = 'none';
+document.querySelector('.downIcon').style.display = 'block';
+});
+company.addEventListener("mouseenter",()=>{
+  document.querySelector('.companyList').style.display = 'flex';
+  document.querySelector('.downIcons').style.display = 'none';
+  document.querySelector('.upIcons').style.display = 'block';
+});
+company.addEventListener("mouseleave",()=>{
+  document.querySelector('.companyList').style.display = 'none';
+document.querySelector('.upIcons').style.display = 'none';
+document.querySelector('.downIcons').style.display = 'block';
 });
