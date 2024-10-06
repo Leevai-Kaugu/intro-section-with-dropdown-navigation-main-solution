9fa1df372622f429b2b67d9d48946cdeb9b0135b
Leevai-Kaugu <125732303+Leevai-Kaugu@users.noreply.github.com>
2024-10-06 13:00:12 +0300

    new changes

--- index.js
+++ index.js
@@ -5,36 +5,35 @@ const features = document.querySelector('.Features');
 const company = document.querySelector('.company');
 const upIcon = document.querySelector('.featuresClose');
 const Klose = document.querySelector('.Klose');
+const overlay =document.querySelector('.overlay')
 
 
 Klose.addEventListener('click',()=>{
   menu.classList.remove('show')
+  overlay.classList.remove('showOverlay')
 })
 hamButton.addEventListener("click",()=>{
-  menu.classList.add('show')
+  menu.classList.add('show');
+  overlay.classList.add('showOverlay')
 });
 
 
 // HOVER STATES
 features.addEventListener("mouseenter",()=>{
   document.querySelector('.featuresList').style.display = 'flex';
-  document.querySelector('.downIcon').style.display = 'none';
-  document.querySelector('.upIcon').style.display = 'block';
+  document.querySelector('.downIcon').style.transform = 'rotate(180deg)';
 });
 features.addEventListener("mouseleave",()=>{
   document.querySelector('.featuresList').style.display = 'none';
-document.querySelector('.upIcon').style.display = 'none';
-document.querySelector('.downIcon').style.display = 'block';
+document.querySelector('.downIcon').style.transform = 'rotate(0deg)';
 });
 company.addEventListener("mouseenter",()=>{
   document.querySelector('.companyList').style.display = 'flex';
-  document.querySelector('.downIcons').style.display = 'none';
-  document.querySelector('.upIcons').style.display = 'block';
+  document.querySelector('.downIcons').style.transform = 'rotate(180deg)';
 });
 company.addEventListener("mouseleave",()=>{
   document.querySelector('.companyList').style.display = 'none';
-document.querySelector('.upIcons').style.display = 'none';
-document.querySelector('.downIcons').style.display = 'block';
+document.querySelector('.downIcons').style.transform = 'rotate(0deg)';
 });
 // HOVER STATES
 
